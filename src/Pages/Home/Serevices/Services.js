04f7dc4224c160a4190cd18720a445a2a0c9a3ef
masud rana <masudranaacb@gmail.com>
2022-04-14 05:27:38 +0600

    meadia qurey added vanila css

--- src/Pages/Home/Serevices/Services.js
+++ src/Pages/Home/Serevices/Services.js
@@ -1,15 +1,24 @@
 import React, { useEffect, useState } from 'react';
-
+import Service from '../Service/Service';
+import './Services.css'
 const Services = () => {
-    const [services,setServices]=useState([])
-    useEffect(()=>{
+    const [services, setServices] = useState([])
+    useEffect(() => {
         fetch('services.json')
-        .then(res=>res.json())
-        .then(data=>setServices(data))
-    },[])
+            .then(res => res.json())
+            .then(data => setServices(data))
+    }, [])
     return (
         <div>
-            <h3>This is Services {services.length}</h3>
+            <h3 className='services-titel'> Services </h3>
+            <div className="services-container">
+                {
+                    services.map(service => <Service
+                        key={service.id}
+                        service={service}
+                    ></Service>)
+                }
+            </div>
         </div>
     );
 };
